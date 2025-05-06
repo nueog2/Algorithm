009efc67034539e2781eb8d5b85053fa6431ed3b
전고은 <132263414+nueog2@users.noreply.github.com>
2025-05-06 23:37:45 +0900

    [Bronze II] Title: 소수 찾기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -3,16 +3,13 @@ nums = list(map(int, input().split()))
 count = 0
 
 for x in nums:
-    if x < 2:
-        continue  
-    is_prime = True
-    for i in range(2, int(x ** 0.5) + 1):
+    if x == 1:
+        continue
+
+    for i in range(2,x):
         if x % i == 0:
-            is_prime = False
             break
-    if is_prime == True:
-    # if is_prime: 으로 적어도 됨.     
+    else:
         count += 1
 
 print(count)
-# 참고, 1 하고 0은 소수가 아님. 
