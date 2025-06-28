b5d11ea53fd568efbb6e8dcbe33d8e3a4a7ef900
전고은 <132263414+nueog2@users.noreply.github.com>
2025-06-28 18:01:01 +0900

    [level 3] Title: 네트워크, Time: 1.18 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,22 +1,18 @@
 def solution(n, computers):
-
-    visited = [0] * (n)
+    
+    visited = [0]*n
     answer = 0
-
-    # dfs 
-    def dfs(c):
-        visited[c] = 1
-
-        for i in range(n):
-            if i != c and computers[c][i] == 1:
-                if visited[i] == 0:
-                    dfs(i)
-
-    # 컴퓨터 하나씩 start 지점으로 다 방문해야함
-    for c in range(n):
-        if visited[c] == 0:
-            dfs(c)
+    
+    def dfs(i):
+        visited[i] = 1
+        for j in range(n):
+            if i!=j and visited[j] == 0 and computers[i][j] == 1:
+                dfs(j)
+    
+    for i in range(n):
+        if visited[i] == 0:
+            dfs(i)
             answer += 1
-            
+    
     return answer
 
